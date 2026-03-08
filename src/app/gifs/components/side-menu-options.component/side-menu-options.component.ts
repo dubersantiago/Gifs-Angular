95c774ccf6ed8e8f9a9e56f67c7d006d91b2678d
duber garcia <dubersi1@gmail.com>
2026-03-08 17:08:29 -0500

    feat: navegacion en el side menu

--- src/app/gifs/components/side-menu-options.component/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options.component/side-menu-options.component.ts
@@ -1,9 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption{
+  label:string,
+  subLabel:string,
+  icon:string,
+  route:string
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent { 
+  menuOptions:MenuOption[]=[
+    {
+      icon:'fa-solid fa-arrow-trend-up',
+      label:'Trending',
+      route:'/dashboard/trending',
+      subLabel:'Gifs populares'
+    },
+    {
+      icon:'fa-solid fa-magnifying-glass',
+      label:'Search',
+      route:'/dashboard/search',
+      subLabel:'Buscar gifs'
+    }
+  ]
+}
