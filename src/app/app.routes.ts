d29e4f93dadfdc36748641a672b010016db8f7a3
duber garcia <dubersi1@gmail.com>
2026-03-08 16:18:21 -0500

    feat: creacion de pages para dsahboard, serach, trendings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardPageComponent } from './gifs/pages/dashboard-page.component/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"dashboard",
+        loadComponent: ()=>import('./gifs/pages/dashboard-page.component/dashboard-page.component').then(c=>c.DashboardPageComponent)
+    },
+    {
+        path:"trending",
+        loadComponent: ()=>import('./gifs/pages/trendingPage.component/trendingPage.component').then(c=>c.TrendingPageComponent)
+    },
+    {
+        path:"search",
+        loadComponent: ()=>import('./gifs/pages/searchPage.component/searchPage.component').then(c=>c.SearchPageComponent)
+    },
+    {
+        path:"**",
+        redirectTo:"dashboard"
+    }
+];
---
+++ src/app/gifs/pages/trendingPage.component/trendingPage.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page.component',
+  imports: [],
+  templateUrl: './trendingPage.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TrendingPageComponent { }
