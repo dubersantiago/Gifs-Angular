0628b0d187afd3aaf3daa98e044999aec355a4ab
duber garcia <dubersi1@gmail.com>
2026-03-08 16:39:16 -0500

    feat: division en componentes el side menu

---
+++ src/app/gifs/components/gifs-sideMenu.component/gifs-sideMenu.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SideMenuHeaderComponent } from "../side-menu-header.component/side-menu-header.component";
+import { SideMenuOptionsComponent } from "../side-menu-options.component/side-menu-options.component";
+
+@Component({
+  selector: 'gifs-side-menu',
+  imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
+  templateUrl: './gifs-sideMenu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GifsSideMenuComponent { }
--- src/app/gifs/pages/dashboard-page.component/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page.component/dashboard-page.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GifsSideMenuComponent } from "../../components/gifs-sideMenu.component/gifs-sideMenu.component";
 
 
 @Component({
   selector: 'app-dashboard-page.component',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GifsSideMenuComponent],
   templateUrl: './dashboard-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
