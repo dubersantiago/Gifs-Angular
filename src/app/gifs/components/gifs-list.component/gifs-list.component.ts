e7a017b95b4a5470bd44e66bdf1023ae2a2c8711
duber garcia <dubersi1@gmail.com>
2026-03-08 23:20:43 -0500

    feat: creacion de componente de gifs de listas

--- src/app/gifs/components/gifs-list.component/gifs-list.component.ts
+++ src/app/gifs/components/gifs-list.component/gifs-list.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item.component/gif-list-item.component";
 
 @Component({
   selector: 'gifs-list',
-  imports: [],
+  imports: [GifListItemComponent],
   templateUrl: './gifs-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/gifs/pages/trendingPage.component/trendingPage.component.ts
+++ src/app/gifs/pages/trendingPage.component/trendingPage.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifsListComponent } from "../../components/gifs-list.component/gifs-list.component";
 
 @Component({
   selector: 'app-trending-page.component',
-  imports: [],
+  imports: [GifsListComponent],
   templateUrl: './trendingPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
