5cc3583783874a6e2c5398cf1cf5c2c0990e7453
duber garcia <dubersi1@gmail.com>
2026-05-11 23:02:27 -0500

    feat: input de arreglo de stings

--- src/app/gifs/components/gifs-list.component/gifs-list.component.ts
+++ src/app/gifs/components/gifs-list.component/gifs-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { GifListItemComponent } from "./gif-list-item.component/gif-list-item.component";
 
 @Component({
@@ -8,20 +8,7 @@ import { GifListItemComponent } from "./gif-list-item.component/gif-list-item.co
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GifsListComponent { 
-  images:string[]=[
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg',
-  ];
+  images=input<string[]>();
 }
 
 
